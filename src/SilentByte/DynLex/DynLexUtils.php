94ffd7670a6bd0fb9609d4674ce77295447b684b
Rico Beti <rico.beti@silentbyte.com>
2016-12-01 11:30:43 +0800

    Slight reformatting.

--- src/SilentByte/DynLex/DynLexUtils.php
+++ src/SilentByte/DynLex/DynLexUtils.php
@@ -48,18 +48,18 @@ class DynlexUtils
         echo "-------------------------------------\n";
 
         echo str_pad('tag', 12, ' ', STR_PAD_RIGHT), '  ',
-             str_pad('off', 4, ' ', STR_PAD_LEFT), '  ',
-             str_pad('ln', 4, ' ', STR_PAD_LEFT), '  ',
-             str_pad('col', 4, ' ', STR_PAD_LEFT), '  ',
-                'value', "\n";
+             str_pad('off',  4, ' ', STR_PAD_LEFT),  '  ',
+             str_pad('ln',   4, ' ', STR_PAD_LEFT),  '  ',
+             str_pad('col',  4, ' ', STR_PAD_LEFT),  '  ',
+             'value', "\n";
 
         echo "-------------------------------------\n";
 
         foreach($tokens as $t) {
             echo str_pad($t->rule->tag(), 12, ' ', STR_PAD_RIGHT), '  ',
-                str_pad($t->offset, 4, ' ', STR_PAD_LEFT), '  ',
-                str_pad($t->line, 4, ' ', STR_PAD_LEFT), '  ',
-                str_pad($t->column, 4, ' ', STR_PAD_LEFT), '  ',
+                str_pad($t->offset,        4, ' ', STR_PAD_LEFT),  '  ',
+                str_pad($t->line,          4, ' ', STR_PAD_LEFT),  '  ',
+                str_pad($t->column,        4, ' ', STR_PAD_LEFT),  '  ',
                 $t->value, "\n";
         }
 
